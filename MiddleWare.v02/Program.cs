ad3f9a9eb7bc522ccde39609752b3a511a64e091
charlipaolan <charlipaolan@gmail.com>
2023-06-02 19:22:36 -0300

    Add project files.

---
+++ MiddleWare.v02/Program.cs
@@ -0,0 +1,31 @@
+using MiddlewareLibrary.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<CustomMiddleware>();
+app.UseMiddleware<CustomMiddlewareStep2>();
+app.UseMiddleware<CustomMiddlewareStep3>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ MiddlewareLibrary/Middleware/CustomMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MiddlewareLibrary.Middleware
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CustomMiddleware> _logger;
+
+        public CustomMiddleware(RequestDelegate next, ILogger<CustomMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Add custom header to the response
+            context.Response.Headers.Add("X-Custom-Header", "Hello from custom middleware!");
+
+            // Log a message
+            _logger.LogInformation("Custom middleware is executing.");
+
+            if (DateTime.Now.Second > 30) context.Response.Headers.Add("Functionalities", "Functionalities OK");
+
+
+            // Call the next middleware in the pipeline
+            await _next(context);
+        }
+    }
+}
